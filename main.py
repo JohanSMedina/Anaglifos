16b20e69ae805066229e182944e859c2d063de8a
JMedR <sebasmedina877@gmail.com>
2023-02-02 13:03:49 -0500

    Se agrega La vision cruzada y paralela totalmente funcional, lo queda por terminar la Top&Donw

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ from PIL import ImageTk
 import os
 import cv2
 import imutils
+import keyboard
 import numpy as np
 import PIL, PIL.Image, PIL.ImageOps, PIL.ImageEnhance
+import time
 
 
 #TODO# Obtencion de imagenes#####
@@ -19,40 +21,81 @@ def ImagenDerecha():
     pathImage = filedialog.askopenfilename(filetypes = [("image", ".jpg")])
 
     if len(pathImage) > 0:
-        global ImDerecha, wid, hgt
+        global ImDerecha, wid, hgt, ImDerechaCV
         ImDerecha = PIL.Image.open(pathImage, mode = 'r').convert('L')
+        ImDerechaCV = cv2.imread(pathImage)
         wid, hgt = ImDerecha.size
 
 def ImagenIzquierda():
     pathImage = filedialog.askopenfilename(filetypes = [("image", ".jpg")])
 
     if len(pathImage) > 0:
-        global ImIzquierda
+        global ImIzquierda, ImIzquierdaCV
         ImIzquierda = PIL.Image.open(pathImage, mode = 'r').convert('L')
+        ImIzquierdaCV = cv2.imread(pathImage)
+        
 
 #?###################
 #?#### Filtrados#####
 #?###################
 
-###Anaglifo###
 def Anaglifo():
     global ImIzquierda, ImDerecha , wid, hgt
     ImRoja = PIL.ImageOps.colorize(ImIzquierda,(0,0,0),(255,0,0))#Las iamgenes estan en BGR
     ImCyan = PIL.ImageOps.colorize(ImDerecha,(0,0,0),(0,255,255))#Limita los espacios de color desde 0 hasta el valor deseado 255 en el canal
 
+
     mezcla = PIL.Image.blend(ImRoja, ImCyan, 0.5)#Alpha 0.5 para que se puperpongan las dos imagenes sin predominancias en ninguna
     npMezcla = np.array(mezcla)
-    ImCombinada = imutils.resize(npMezcla, height=hgt)
-    ImCombinada = cv2.cvtColor(ImCombinada, cv2.COLOR_BGR2RGB)
 
-    cv2.imwrite("Resultado.jpg",ImCombinada)#Guardar la imagen en el computador
-    os.startfile("Resultado.jpg")#Abrir la imagen con el gestor de imagenes del computador
+    ImCombinada = imutils.resize(npMezcla, height=hgt)
+    ImCombinada = cv2.cvtColor(ImCombinada, cv2.COLOR_BGR2RGB)#Al pasar de BGR a RGB solo cambia el formato de la imagen, pero la informacion de los pixeles queda igual en el mismo orden
+
+    os.chdir("Resultados")
+    cv2.imwrite("Anaglifo.jpg",ImCombinada)#Guardar la imagen en el computador
+    os.startfile("Anaglifo.jpg")#Abrir la imagen con el gestor de imagenes del computador
+    os.chdir("..")
+
+    time.sleep(0.2)##Esperar y maximizar imagen 
+    keyboard.press('F11')
 
 def Paralela():
-    global ImIzquierda, ImDerecha
-    ImRoja = PIL.ImageOps.colorize(ImIzquierda,(0,0,0),(255,0,0))
-    ImCyan = PIL.ImageOps.colorize(ImDerecha,(0,0,0),(0,255,255))
-    cv2.imshow('Paralela', ImCyan)
+    global ImIzquierdaCV, ImDerechaCV
+
+    os.chdir("Resultados")
+    cv2.imwrite("VisionParalela.jpg", np.hstack([ImIzquierdaCV,ImDerechaCV]))
+    os.startfile("VisionParalela.jpg")#Abrir la imagen con el gestor de imagenes del computador
+    os.chdir("..")
+
+    time.sleep(0.2)
+    keyboard.press('F11')
+
+def Cruzada():
+    global ImIzquierdaCV, ImDerechaCV
+
+    os.chdir("Resultados")
+    cv2.imwrite("VisionCruzada.jpg", np.hstack([ImDerechaCV,ImIzquierdaCV]))
+    os.startfile("VisionCruzada.jpg")#Abrir la imagen con el gestor de imagenes del computador
+    os.chdir("..")
+
+    time.sleep(0.2)
+    keyboard.press('F11')
+
+def PruebaAnaglifos():#
+    global ImIzquierda, ImDerecha , wid, hgt
+    ImRoja = PIL.ImageOps.colorize(ImIzquierda,(0,0,0),(255,0,0))#Las iamgenes estan en BGR
+    ImCyan = PIL.ImageOps.colorize(ImDerecha,(0,0,0),(0,255,255))#Limita los espacios de color desde 0 hasta el valor deseado 255 en el canal
+    npImRoja = np.array(ImRoja)
+    npImCyan = np.array(ImCyan)
+    
+    npImRoja = imutils.resize(npImRoja, height=hgt)
+    npImCyan = imutils.resize(npImCyan, height=hgt)
+    npImRoja1 = cv2.cvtColor(npImRoja, cv2.COLOR_BGR2RGB)
+    npImCyan1 = cv2.cvtColor(npImCyan, cv2.COLOR_BGR2RGB)
+    cv2.imwrite("Canalesseparados.jpg", np.hstack([npImRoja,npImCyan,npImRoja1,npImCyan1]))
+    os.startfile("Canalesseparados.jpg")#Abrir la imagen con el gestor de imagenes del computador
+    time.sleep(0.1)
+    keyboard.press('F11')
 
 
 
@@ -76,7 +119,7 @@ btnAnaglifo.grid(column=0, row=2, padx=5, pady = 5)
 btnParalela = Button(root, text = "Vision paralela", width = 18, font=("Palatino Linotype",13), command=Paralela)
 btnParalela.grid(column=1, row=2, padx=5, pady = 5)
 
-btnCruzada = Button(root, text = "Vision cruzada", width = 18, font=("Palatino Linotype",13))
+btnCruzada = Button(root, text = "Vision cruzada", width = 18, font=("Palatino Linotype",13),command=Cruzada)
 btnCruzada.grid(column=0, row=3, padx=5, pady = 5)
 
 btnTD = Button(root, text = "Top & Down", width = 18, font=("Palatino Linotype",13))
